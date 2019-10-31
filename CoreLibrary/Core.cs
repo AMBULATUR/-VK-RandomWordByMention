3c57dcb3e760112ffc5c2c20781f09781a338624
AMBULATUR <ambulatur@gmail.com>
2019-11-01 01:34:09 +0300

    Last update. No perspective

---
+++ CoreLibrary/Core.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using VkNet;
+using VkNet.AudioBypassService.Extensions;
+using VkNet.Model;
+using VkNet.NLog.Extensions.Logging;
+using VkNet.NLog.Extensions.Logging.Extensions;
+
+namespace CoreLibrary
+{
+    public class Core
+    {
+        static private long? idChat;
+        static private long? messageId;
+        static ServiceCollection services = new ServiceCollection();
+        static public VkApi api = null;
+        static private long? randomId = 0;
+
+        /// <summary>
+        /// Авторизация
+        /// </summary>
+        /// <param name="vkLogin">Логин</param>
+        /// <param name="vkPassword">Пароль</param>
+        /// <param name="LoggingEnabled">Логгирование действий библиотеки VkNet. По умолчанию - выключено</param>
+        public static void VkAuth(string vkLogin, string vkPassword, bool LoggingEnabled = false)
+        {
+            services.AddAudioBypass();
+            #region Logger
+            // Регистрация логгера
+            if (LoggingEnabled)
+            {
+                services.AddSingleton<ILoggerFactory, LoggerFactory>();
+                services.AddSingleton(typeof(ILogger<>), typeof(Logger<>));
+                services.AddLogging(builder =>
+                {
+                    builder.ClearProviders();
+                    builder.SetMinimumLevel(LogLevel.Trace);
+                    builder.AddNLog(new NLogProviderOptions
+                    {
+                        CaptureMessageProperties = true,
+                        CaptureMessageTemplates = true
+                    });
+                });
+                NLog.LogManager.LoadConfiguration("nlog.config");
+            }
+            #endregion
+
+            api = new VkApi(services);
+            api.Authorize(new ApiAuthParams
+            {
+                Login = vkLogin,
+                Password = vkPassword
+            });
+        }
+        /// <summary>
+        /// Поиск упоминания заданной строки
+        /// </summary>
+        /// <param name="query">Искомая строка</param>
+        public static bool FindMention(string query)
+        {
+            var MentionList = api.Messages.Search(new VkNet.Model.RequestParams.MessagesSearchParams
+            {
+                Query = query,
+                PreviewLength = 0,
+                Offset = 0,
+                Count = 3,
+                Extended = true,
+            });
+            idChat = MentionList.Items[0].PeerId - 2000000000;
+            messageId = MentionList.Items[0].Id;
+            if (randomId == MentionList.Items[0].RandomId)
+            {
+                return false;
+            }
+            else
+            {
+                randomId = MentionList.Items[0].RandomId;
+                return true;
+            }
+        }
+        /// <summary>
+        /// Ответ на сообщение крайнему пользователю инициализированным методом FindMention(string query)
+        /// </summary>
+        /// <param name="message">Ответное сообщение</param>
+        public static void SendMessage(string message)
+        {
+            List<long> messages = new List<long>() { (long)messageId };
+            api.Messages.Send(new VkNet.Model.RequestParams.MessagesSendParams
+            {
+              //  ForwardMessages = messages, //Переслать сообщение пользователя вызвавшего бота
+                ChatId = idChat,
+                Message = message,
+                RandomId = api.GetHashCode() * DateTime.Now.Millisecond
+            });
+        }
+    }
+}
--- main.Designer.cs
+++ main.Designer.cs
@@ -32,22 +32,22 @@
             this.PasswordBox = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.EnableButton = new System.Windows.Forms.Button();
-            this.IdBox = new System.Windows.Forms.TextBox();
+            this.AuthButton = new System.Windows.Forms.Button();
+            this.QueryBox = new System.Windows.Forms.TextBox();
             this.Chatlabel = new System.Windows.Forms.Label();
-            this.SendButton = new System.Windows.Forms.Button();
+            this.EnableButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // LoginBox
             // 
-            this.LoginBox.Location = new System.Drawing.Point(35, 38);
+            this.LoginBox.Location = new System.Drawing.Point(12, 25);
             this.LoginBox.Name = "LoginBox";
             this.LoginBox.Size = new System.Drawing.Size(100, 20);
             this.LoginBox.TabIndex = 0;
             // 
             // PasswordBox
             // 
-            this.PasswordBox.Location = new System.Drawing.Point(35, 77);
+            this.PasswordBox.Location = new System.Drawing.Point(12, 64);
             this.PasswordBox.Name = "PasswordBox";
             this.PasswordBox.Size = new System.Drawing.Size(100, 20);
             this.PasswordBox.TabIndex = 1;
@@ -55,7 +55,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(32, 22);
+            this.label1.Location = new System.Drawing.Point(9, 9);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(29, 13);
             this.label1.TabIndex = 2;
@@ -64,63 +64,63 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(32, 61);
+            this.label2.Location = new System.Drawing.Point(9, 48);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(29, 13);
             this.label2.TabIndex = 3;
             this.label2.Text = "pass";
             // 
-            // EnableButton
+            // AuthButton
             // 
-            this.EnableButton.Location = new System.Drawing.Point(35, 121);
-            this.EnableButton.Name = "EnableButton";
-            this.EnableButton.Size = new System.Drawing.Size(75, 23);
-            this.EnableButton.TabIndex = 4;
-            this.EnableButton.Text = "Enable";
-            this.EnableButton.UseVisualStyleBackColor = true;
-            this.EnableButton.Click += new System.EventHandler(this.EnableButton_Click);
+            this.AuthButton.Location = new System.Drawing.Point(12, 90);
+            this.AuthButton.Name = "AuthButton";
+            this.AuthButton.Size = new System.Drawing.Size(75, 23);
+            this.AuthButton.TabIndex = 4;
+            this.AuthButton.Text = "Auth";
+            this.AuthButton.UseVisualStyleBackColor = true;
+            this.AuthButton.Click += new System.EventHandler(this.AuthButton_Click);
             // 
-            // IdBox
+            // QueryBox
             // 
-            this.IdBox.Location = new System.Drawing.Point(189, 37);
-            this.IdBox.Name = "IdBox";
-            this.IdBox.Size = new System.Drawing.Size(100, 20);
-            this.IdBox.TabIndex = 5;
-            this.IdBox.Text = "@zkarasukz";
+            this.QueryBox.Location = new System.Drawing.Point(189, 37);
+            this.QueryBox.Name = "QueryBox";
+            this.QueryBox.Size = new System.Drawing.Size(100, 20);
+            this.QueryBox.TabIndex = 5;
+            this.QueryBox.Text = "@vkid";
             // 
             // Chatlabel
             // 
             this.Chatlabel.AutoSize = true;
             this.Chatlabel.Location = new System.Drawing.Point(186, 21);
             this.Chatlabel.Name = "Chatlabel";
-            this.Chatlabel.Size = new System.Drawing.Size(38, 13);
+            this.Chatlabel.Size = new System.Drawing.Size(42, 13);
             this.Chatlabel.TabIndex = 6;
-            this.Chatlabel.Text = "yourID";
+            this.Chatlabel.Text = "RelyOn";
             // 
-            // SendButton
+            // EnableButton
             // 
-            this.SendButton.Location = new System.Drawing.Point(189, 121);
-            this.SendButton.Name = "SendButton";
-            this.SendButton.Size = new System.Drawing.Size(75, 23);
-            this.SendButton.TabIndex = 7;
-            this.SendButton.Text = "Send";
-            this.SendButton.UseVisualStyleBackColor = true;
-            this.SendButton.Click += new System.EventHandler(this.SendButton_Click);
+            this.EnableButton.Location = new System.Drawing.Point(189, 63);
+            this.EnableButton.Name = "EnableButton";
+            this.EnableButton.Size = new System.Drawing.Size(75, 23);
+            this.EnableButton.TabIndex = 7;
+            this.EnableButton.Text = "Enable";
+            this.EnableButton.UseVisualStyleBackColor = true;
+            this.EnableButton.Click += new System.EventHandler(this.EnableButton_Click);
             // 
-            // Form1
+            // main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(509, 180);
-            this.Controls.Add(this.SendButton);
-            this.Controls.Add(this.Chatlabel);
-            this.Controls.Add(this.IdBox);
             this.Controls.Add(this.EnableButton);
+            this.Controls.Add(this.Chatlabel);
+            this.Controls.Add(this.QueryBox);
+            this.Controls.Add(this.AuthButton);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.PasswordBox);
             this.Controls.Add(this.LoginBox);
-            this.Name = "Form1";
+            this.Name = "main";
             this.Text = "Form1";
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -133,10 +133,10 @@
         private System.Windows.Forms.TextBox PasswordBox;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Button EnableButton;
-        private System.Windows.Forms.TextBox IdBox;
+        private System.Windows.Forms.Button AuthButton;
+        private System.Windows.Forms.TextBox QueryBox;
         private System.Windows.Forms.Label Chatlabel;
-        private System.Windows.Forms.Button SendButton;
+        private System.Windows.Forms.Button EnableButton;
     }
 }
 
